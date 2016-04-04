de86a71a4eb67e1388370b6d150ad974fad384ed
YiTaiChen <jonathanchen16@gmail.com>
2016-04-04 19:48:53 +1000

    Part 2 a

---
+++ app/src/main/java/com/sample/demo/csci342a1/TileView.java
@@ -0,0 +1,15 @@
+package com.sample.demo.csci342a1;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by ChenYiTai on 4/4/16.
+ */
+public class TileView extends LinearLayout {
+    public TileView(Context context,AttributeSet attrs)
+    {
+        super(context,attrs);
+    }
+}
