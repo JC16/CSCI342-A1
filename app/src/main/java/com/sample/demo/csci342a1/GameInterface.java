9adda079b26dc00ef692824c866c465abc1776f6
YiTaiChen <jonathanchen16@gmail.com>
2016-04-04 15:21:47 +1000

    Part 1

---
+++ app/src/main/java/com/sample/demo/csci342a1/GameInterface.java
@@ -0,0 +1,13 @@
+package com.sample.demo.csci342a1;
+
+/**
+ * Created by ChenYiTai on 4/4/16.
+ */
+public interface GameInterface {
+
+    void gameDidComplete(GameModel gameModel);
+    void didMatchTile(GameModel gameModel, int tileIndex, int previousTileIndex);
+    void didFailToMatchTile(GameModel gameModel,int tileIndex,int previousTileIndex);
+    void scoreDidUpdate(GameModel gameModel,int newScore);
+
+}
