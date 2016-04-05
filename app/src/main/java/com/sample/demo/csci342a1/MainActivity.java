5c6b7552f72e1e21a76fb2d709574f71822ec9c4
YiTaiChen <jonathanchen16@gmail.com>
2016-04-05 23:12:51 +1000

    Part 4

--- app/src/main/java/com/sample/demo/csci342a1/MainActivity.java
+++ app/src/main/java/com/sample/demo/csci342a1/MainActivity.java
@@ -1,33 +1,99 @@
 package com.sample.demo.csci342a1;
 
+import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity implements TileView.TileViewInterface, GameInterface  {
+
+    private GameModel gm;
+    private ArrayList<TileView> tileView = new ArrayList<>();
+    private final int numOfImg =12;
+    private TileView Tview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        int numOfImg = 6;
         Drawable ImgDrawable[] = {getResources().getDrawable(R.drawable.lake),getResources().getDrawable(R.drawable.cathedral)
                 ,getResources().getDrawable(R.drawable.baldhill)};
 
-        GameModel gm = new GameModel(numOfImg, ImgDrawable);
+        gm = new GameModel(numOfImg, ImgDrawable);
 
-        Log.e("MainActivity","Testing");
+
+
+        for(int i=0; i<numOfImg; i++)
+        {
+            try {
+                int id = R.id.class.getField("View"+i).getInt(0);
+                Tview = (TileView)findViewById(id);
+                tileView.add(Tview);
+
+                tileView.get(i).revailImage(gm.TileList.get(i).photo);
+                tileView.get(i).setTileIndex(i);
+                tileView.get(i).setInterface(this);
+                tileView.get(i).coverImage();
+
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (NoSuchFieldException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        Log.e("MainActivity", "Testing");
         Log.e("MainActivity", gm.description());
+        Log.e("MainActivity", Integer.toString(tileView.size()));
 
 
 
+    }
 
+    @Override
+    public void didSelectTile(TileView tileView) {
+            int lastTap = tileView.getTileIndex();
+
+            gm.pushTileIndex(lastTap);
+
+
+    }
+
+    @Override
+    public void gameDidComplete(GameModel gameModel) {
+
+    }
+
+    @Override
+    public void didMatchTile(GameModel gameModel, int tileIndex, int previousTileIndex) {
+
+        if(gameModel.TileList.get(tileIndex).ImgId == gameModel.TileList.get(previousTileIndex).ImgId)
+        {
+            tileView.get(tileIndex).hideTile();
+            tileView.get(previousTileIndex).hideTile();
+        }
+
+    }
+
+    @Override
+    public void didFailToMatchTile(GameModel gameModel, int tileIndex, int previousTileIndex) {
+        if(gameModel.TileList.get(tileIndex).ImgId != gameModel.TileList.get(previousTileIndex).ImgId)
+        {
+            tileView.get(tileIndex).coverImage();
+            tileView.get(previousTileIndex).coverImage();
+        }
+    }
+
+    @Override
+    public void scoreDidUpdate(GameModel gameModel, int newScore) {
 
     }
 }
--- app/src/main/java/com/sample/demo/csci342a1/TileView.java
+++ app/src/main/java/com/sample/demo/csci342a1/TileView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,7 +19,25 @@ public class TileView extends LinearLayout {
 
     private ImageView imgView;
 
+    public int getTileIndex() {
+        return tileIndex;
+    }
+
+    public void setTileIndex(int tileIndex) {
+        this.tileIndex = tileIndex;
+    }
+
+    public ImageView getImgView() {
+        return imgView;
+    }
+
+    public void setImgView(ImageView imgView) {
+        this.imgView = imgView;
+    }
+
     private int tileIndex;
+
+    private TileViewInterface tileViewInterface;
 
     public interface TileViewInterface
     {
@@ -35,13 +54,14 @@ public class TileView extends LinearLayout {
 
         this.addView(imgView);
 
-
+/*
         this.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 imgView.setImageResource(R.drawable.lake);
+                coverImage();
             }
-        });
+        });*/
 
     }
 
@@ -51,8 +71,8 @@ public class TileView extends LinearLayout {
             @Override
             public void onClick(View v) {
 
-                //imgView.setImageDrawable(usedImg);
-                imgView.setImageResource(R.drawable.lake);
+                imgView.setImageDrawable(usedImg);
+                //imgView.setImageResource(R.drawable.lake);
 
             }
         });
@@ -60,22 +80,31 @@ public class TileView extends LinearLayout {
     }
     public void coverImage()
     {
-        this.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
                 imgView.setImageResource(R.drawable.question);
-            }
-        });
     }
 
-    public void hideTile(boolean match)
+    public void hideTile()
     {
-        if(match)
-        {
+
             this.setBackgroundColor(Color.WHITE);
-        }
+            imgView.setImageDrawable(null);
     }
 
+    public void nextViewToLand()
+    {
+        if(tileViewInterface == null)
+        {
+            Log.e("Error", "No listener set");
+            return;
+        }
+        tileViewInterface.didSelectTile(this);
+    }
+
+
+    public void setInterface(TileViewInterface tileViewInterface)
+    {
+        this.tileViewInterface = tileViewInterface;
+    }
 
 
 }
